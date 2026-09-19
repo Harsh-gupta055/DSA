330ae1e9e0b32ef73968d8ea0237e12dcb45aaeb
Harsh-gupta055 <omshop15681501@gmail.com>
2026-09-19 23:20:20 +0530

    Time: 3 ms (59.9%), Space: 15.5 MB (79.03%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -4,12 +4,14 @@ public:
       int count=0 ;
       int numrow =strs.size() ;
       int numcol =strs[0].size() ;
-
+      //loop through each column
       for(int col=0 ;col<numcol ;col++ ){
+        //compare each character  with the one above it 
         for(int row=1 ;row<numrow ; row++){
             if(strs[row][col]< strs[row-1][col]){
                 count++ ;
                 break ;
+                //move to next column once an out of order pair is found 
             }
         }
       }
